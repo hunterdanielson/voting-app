c0b333f7661ce2fb39d17374b407b50434e1cf54
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-10 15:34:34 -0700

    get poll by id pass

--- __tests__/polls.test.js
+++ __tests__/polls.test.js
@@ -14,6 +14,11 @@ describe('poll routes', () => {
     return connect(uri);
   });
 
+
+  beforeEach(async() => {
+    return mongoose.connection.dropDatabase();
+  });
+
   let organization;
   beforeEach(async() => {
     organization = await Organization.create({
@@ -21,13 +26,13 @@ describe('poll routes', () => {
       description: 'we make water',
       imageUrl: 'water.png'
     });
-    return mongoose.connection.dropDatabase();
   });
 
   afterAll(async() => {
     await mongoose.connection.close();
     return mongod.stop();
   });
+  
   it('creates a poll via POST', () => {
     return request(app)
       .post('/api/v1/polls')
@@ -40,7 +45,7 @@ describe('poll routes', () => {
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
-          organization: expect.anything(),
+          organization: `${organization._id}`,
           title: 'water poll',
           description: 'do you drink water',
           options: ['Yes', 'No'],
@@ -65,55 +70,26 @@ describe('poll routes', () => {
       });
   });
 
-  // it('updates a poll by id via PATCH', () => {
-  //   return Poll.create({
-  //     name: 'hunter',
-  //     phone: '1234567890',
-  //     email: 'fakeemail@gmail.com',
-  //     communicationMedium: 'phone',
-  //     imageUrl: 'pic.png'
-  //   })
-  //     .then(poll => {
-  //       return request(app)
-  //         .patch(`/api/v1/polls/${poll._id}`)
-  //         .send({ email: 'newfakeemail@gmail.com' });
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.anything(),
-  //         name: 'hunter',
-  //         phone: '1234567890',
-  //         email: 'newfakeemail@gmail.com',
-  //         communicationMedium: 'phone',
-  //         imageUrl: 'pic.png',
-  //         __v: 0
-  //       });
-  //     });
-  // });
-
-  // it('can del a poll by id via DELETE', () => {
-  //   return Poll.create({
-  //     name: 'hunter',
-  //     phone: '1234567890',
-  //     email: 'fakeemail@gmail.com',
-  //     communicationMedium: 'phone',
-  //     imageUrl: 'pic.png'
-  //   })
-  //     .then(poll => {
-  //       return request(app)
-  //         .delete(`/api/v1/polls/${poll._id}`)
-  //         .send({ imageUrl: 'otherpic.png' });
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.anything(),
-  //         name: 'hunter',
-  //         phone: '1234567890',
-  //         email: 'fakeemail@gmail.com',
-  //         communicationMedium: 'phone',
-  //         imageUrl: 'pic.png',
-  //         __v: 0
-  //       });
-  //     });
-  // });
+  it('gets specific poll by id via GET', () => {
+    return Poll.create({
+      organization: organization._id,
+      title: 'water poll',
+      description: 'You drink water',
+      options: ['Yes', 'No']
+    })
+      .then(poll => request(app).get(`/api/v1/polls/${poll._id}`))
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          organization: {
+            _id: organization.id,
+            title: 'water company'
+          },
+          title: 'water poll',
+          description: 'You drink water',
+          options: ['Yes', 'No'],
+          __v: 0 
+        });
+      });
+  });
 });
